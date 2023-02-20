c61e693bfef12e7dd643fb796e2d4a22d779f8e5
Florian <florian.sasso@outlook.fr>
2023-02-20 17:34:41 +0100

    Models dependancies and validations set

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+    has_many :attendances
+    has_many :users, through: :attendances
+    belongs_to :user
+    validates :start_date, presence: true #, No create of update before actual date
+    validates :duration, presence: true #, positif, multiple of 5 and in minutes
+    validates :title, presence: true, length: { in: 5..140 }
+    validates :description, presence: true, length: { in: 20..1000 }
+    validates :price, presence: true, length: { in: 1..1000 }
+    validates :location, presence: true
 end
