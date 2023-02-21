1d095d002da7f3dc6b2069435f82ccfbde63ea74
Florian <florian.sasso@outlook.fr>
2023-02-21 14:24:18 +0100

    events routes / controller / View index as website root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/create'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'events#index'
+
+  resources :events
 end
